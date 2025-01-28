85cc1c41c32361b10814d7a4c44f415591afc513
SaraToth <saratoth101@gmail.com>
2025-01-28 21:58:35 +0900

    Feat: Write program 1-2
    Program 1-2 outputs a text of CS! with a message Computer science is cool stuff!

--- chapter-1-programs/1-2.cpp
+++ chapter-1-programs/1-2.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 
 int main() {
+    cout<<"***************************************** \n \n";
+    cout<<"      C C C          S S S S         !! \n";
+    cout<<"    C       C       S       S        !! \n";
+    cout<<"   C               S                 !! \n";
+    cout<<"  C                 S                !! \n";
+    cout<<"  C                  S S S S         !! \n";
+    cout<<"  C                         S        !! \n";
+    cout<<"   C                         S       !! \n";
+    cout<<"    C       C       S       S \n";
+    cout<<"      C C C          S S S S         00 \n \n";
+    cout<<"***************************************** \n \n";
+    cout<<"   Computer science is cool stuff! \n \n";
 
     return 0;
 }
+/*
+OUTPUT: 
+
+*****************************************
+
+      C C C          S S S S         !!
+    C       C       S       S        !!
+   C               S                 !!
+  C                 S                !!
+  C                  S S S S         !!
+  C                         S        !!
+   C                         S       !!
+    C       C       S       S
+      C C C          S S S S         00
+
+*****************************************
+
+   Computer science is cool stuff!
+
+*/
