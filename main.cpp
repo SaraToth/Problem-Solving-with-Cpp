bbda383c6557b6a63ef63c60911bfceef0cc8748
SaraToth <saratoth101@gmail.com>
2025-01-28 21:14:13 +0900

    Feat: Define variables, and input numbers
    Input the first and second number which will be utilized to calculate a sum and a product.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
+    int sum, product, first_num, second_num;
+
+    cout<<"Press enter after entering a number. \n";
+    cout<<"Enter a number: \n";
+    cin>>first_num;
+    cout<<"Enter a second number: \n";
+    cin>>second_num;
+
+
 
     return 0;
 }
