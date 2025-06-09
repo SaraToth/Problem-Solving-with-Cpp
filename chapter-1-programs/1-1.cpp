6a7f352072666672aebd3af4ba6b58c2f48446f0
SaraToth <saratoth101@gmail.com>
2025-06-09 18:36:44 +0900

    Feat: Simplify chapter 1-1 cout statements

--- chapter-1-programs/1-1.cpp
+++ chapter-1-programs/1-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int sum, product, first_num, second_num;
 
-    cout<<"Press enter after entering a number. \n";
+    cout<<"Press enter after entering each number. \n";
     cout<<"Enter a number: \n";
     cin>>first_num;
     cout<<"Enter a second number: \n";
@@ -13,13 +13,8 @@ int main() {
     sum = first_num + second_num;
     product = first_num * second_num;
 
-    cout<<"The sum of these numbers is ";
-    cout<<sum;
-    cout<<"\n The product of these numbers is ";
-    cout<<product;
-    cout<<"\n";
-
-
+    cout<<"The sum of these numbers is " << sum << "\n";
+    cout<<"The product of these numbers is " << product << "\n";
 
     return 0;
 }
